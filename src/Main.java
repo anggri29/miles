39418bc2d51ca5d85d3bb1870ebdf920cc71b0fc
Angelina <angry.anggri@gmail.com>
2023-04-10 14:49:10 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int amount = 11899;
+        int bonus = 20;
+        int result = amount / bonus;
+
+    
+
+        System.out.println ("Бонусные мили: " + result);
+    }
+}
